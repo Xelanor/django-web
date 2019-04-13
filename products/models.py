c27bc4897d688987b4972f6fe2123f95ab500468
Xelanor <berozel@hotmail.com>
2019-04-13 16:44:58 +0300

    changes

--- products/models.py
+++ products/models.py
@@ -1,12 +1,56 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
+import random
+import os
 from django.db import models
+from django.db.models.signals import pre_save, post_save
+from .utils import unique_slug_generator
+from django.urls import reverse
+
+def get_filename_ext(filepath):
+    base_name = os.path.basename(filepath)
+    name, ext = os.path.splitext(base_name)
+    return name, ext
+
+def upload_image_path(instance, filename):
+    print(instance)
+    print(filename)
+    new_filename = random.randint(123165, 5416513218)
+    name, ext = get_filename_ext(filename)
+    final_filename = f'{new_filename}{ext}'
+    return f'products/{new_filename}/{final_filename}'
+
+class ProductManager(models.Manager):
+    def feautured(self):
+        return self.get_queryset().filter(feautured=True)
+
+    def get_by_id(self, id):
+        qs = self.get_queryset().filter(id=id)
+        if qs.count() == 1:
+            return qs.first()
+        return None
+
 
 class Product(models.Model):
     title           = models.CharField(max_length = 30)
+    slug            = models.SlugField(default="abc", unique=True)
     description     = models.TextField()
     price           = models.DecimalField(decimal_places=2, max_digits=10, default=39.99)
+    image           = models.ImageField(upload_to=upload_image_path, null=True, blank=True)
+    feautured       = models.BooleanField(default=False)
+    timestamp       = models.DateTimeField(auto_now_add=True)
+
+    def get_absolute_url(self):
+        return reverse("products:detail", kwargs={"slug" : self.slug})
 
     def __str__(self):
         return self.title
+
+    objects = ProductManager()
+
+def product_pre_save_receiver(sender, instance, *args, **kwargs):
+    if not instance.slug:
+        instance.slug = 'abc'
+
+pre_save.connect(product_pre_save_receiver, sender=Product)
--- products/views.py
+++ products/views.py
@@ -1,20 +1,53 @@
 from __future__ import unicode_literals
 
+from django.http import Http404
 from django.views.generic import ListView, DetailView
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 
 from .models import Product
 
 
-class ProductListView(ListView):
-    queryset = Product.objects.all()
+class ProductFeaturedListView(ListView):
     template_name = 'products/list.html'
 
-class ProductDetailView(DetailView):
+    def get_queryset(self, *args, **kwargs):
+        request = self.request
+        return Product.objects.feautured()
+
+class ProductFeaturedDetailView(DetailView):
     queryset = Product.objects.all()
+    template_name = 'products/featured-detail.html'
+
+class ProductListView(ListView):
+    template_name = 'products/list.html'
+
+    def get_queryset(self, *args, **kwargs):
+        request = self.request
+        return Product.objects.all()
+
+class ProductDetailSlugView(DetailView):
+    queryset = Product.objects.all()
+    template_name = 'products/detail.html'
+
+    def get_object(self, *args, **kwargs):
+        request = self.request
+        slug = self.kwargs.get('slug')
+        instance = get_object_or_404(Product, slug=slug)
+        return instance
+
+class ProductDetailView(DetailView):
+    #queryset = Product.objects.all()
     template_name = 'products/detail.html'
 
     def get_context_data(self, *args, **kwargs):
         context = super(ProductDetailView, self).get_context_data(*args, **kwargs)
         print(context)
         return context
+
+    def get_object(self, *args, **kwargs):
+        request = self.request
+        pk = self.kwargs.get('pk')
+        instance = Product.objects.get_by_id(pk)
+        if instance is None:
+            raise Http404("Product doesn't exist")
+        return instance
